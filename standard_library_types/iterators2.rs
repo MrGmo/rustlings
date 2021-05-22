caf6b1c6e908f70e6db9f2bea6d5af02145a31d3
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-21 22:56:31 -0500

    rustling exercises

---
+++ standard_library_types/iterators2.rs
@@ -0,0 +1,46 @@
+// iterators2.rs
+// In this exercise, you'll learn some of the unique advantages that iterators
+// can offer. Follow the steps to complete the exercise.
+// As always, there are hints if you execute `rustlings hint iterators2`!
+
+
+pub fn capitalize_first(input: &str) -> String {
+    let mut c = input.chars();
+    match c.next() {
+        None => String::new(),
+        Some(first) => format!("{}{}", first.to_uppercase(), c.as_str()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(capitalize_first("hello"), "Hello");
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(capitalize_first(""), "");
+    }
+
+    #[test]
+    fn test_iterate_string_vec() {
+        let words = vec!["hello", "world"];
+        let capitalized_words: Vec<String> = words.iter().map(|w| capitalize_first(w)).collect(); // TODO
+        assert_eq!(capitalized_words, ["Hello", "World"]);
+    }
+
+    #[test]
+    fn test_iterate_into_string() {
+        let words = vec!["hello", " ", "world"];
+        let capitalized_words = words
+            .iter()
+            .map(|w| capitalize_first(w))
+            .fold(String::new(), |acc, w| format!("{}{}", acc, w));
+
+        assert_eq!(capitalized_words, "Hello World");
+    }
+}
